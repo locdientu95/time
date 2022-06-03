4f0bcd382cf0028635f7c30cb722762fd3f9929a
Loc <locdientu95@gmail.com>
2022-06-03 14:17:51 +0700

    mycode

---
+++ src/getTime.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+public class getTime  extends Thread {
+    private final Myframe myframe;
+    public getTime(Myframe myframe){
+        this.myframe = myframe;
+    }
+    @Override
+    public void run() {
+        super.run();
+        while(true) {
+            myframe.Date.setText(myframe.dateFormat.format(new Date()));
+            myframe.Time.setText(myframe.timeFormat.format(new Date()));
+            if(myframe.stop){
+                break;
+            }
+        }
+    }
+}
